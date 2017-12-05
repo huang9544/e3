0ff3d853993bb01d8e5c2191b0c88100c18a3d1a
huanggeng <huanggeng@huang.lan>
2017-12-05 20:06:44 +0800

    manager2

--- e3_manager/e3_manager_interface/src/main/java/cn/e3/service/ItemService.java
+++ e3_manager/e3_manager_interface/src/main/java/cn/e3/service/ItemService.java
@@ -1,8 +1,10 @@
 package cn.e3.service;
 
 import cn.e3.pojo.TbItem;
+import cn.e3.utils.DatagridPageBean;
 
 public interface ItemService {
 
 	public TbItem findById(Long itemId);
+	public DatagridPageBean findByPage(Integer page, Integer rows);
 }
--- e3_manager/e3_manager_service/src/main/java/cn/e3/service/impl/ItemServiceImpl.java
+++ e3_manager/e3_manager_service/src/main/java/cn/e3/service/impl/ItemServiceImpl.java
@@ -1,11 +1,19 @@
 package cn.e3.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 import cn.e3.mapper.TbItemMapper;
 import cn.e3.pojo.TbItem;
+import cn.e3.pojo.TbItemCatExample;
+import cn.e3.pojo.TbItemExample;
 import cn.e3.service.ItemService;
+import cn.e3.utils.DatagridPageBean;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -16,6 +24,22 @@ public class ItemServiceImpl implements ItemService {
 	public TbItem findById(Long itemId) {
 		TbItem tbItem = tbItemMapper.selectByPrimaryKey(itemId);
 		return tbItem;
+	}
+	@Override
+	public DatagridPageBean findByPage(Integer page, Integer rows) {
+		DatagridPageBean pageBean = new DatagridPageBean();
+		TbItemExample example = new TbItemExample();
+		
+		PageHelper.startPage(page, rows);
+		List<TbItem> list = tbItemMapper.selectByExample(example);
+		
+		PageInfo<TbItem> pageInfo = new PageInfo<TbItem>(list);
+		long total = pageInfo.getTotal();
+		
+		pageBean.setTotal(total);
+		pageBean.setRows(list);
+		
+		return pageBean;
 	}
 
 }
